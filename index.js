6b3bb3b10d142038c32fb82ce7acb5f43e87a364
Kevin Sedgley <kevin.sedgley@localgiving.com>
2017-04-07 13:08:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
-
-var http = require('http');
+var http = require('http'),
+    url = require('url'),
+    tache = require('mustache');
+    fs = require('fs');
 
 http.createServer(function (request, response) {
+    console.log('Request started');
     response.writeHead(200, {'Content-Type': 'text/plain'});
 
-    var script = require('./script.json');
+    fs.readFile('./script.json', 'utf8', function(err, data) {
+        if (err) {
+            console.log('Error reading');
+            response.write("Couldn\'t read script");
+            response.end();
+            return;
+        }
+        var urlParts = url.parse(request.url, true).query;
+        var script = JSON.parse(tache.render(data, urlParts));
 
+        var spawn = require('child_process').spawn,
+        proc = spawn(script.script, (script.args ? script.args : []));
 
-    var spawn = require('child_process').spawn,
-    proc = spawn(script.script, (script.args ? script.args : []));
+        proc.stdout.on('data', function (data) {
+            response.write('stdout: ' + data.toString());
+        });
 
-    proc.stdout.on('data', function (data) {
-        response.write('stdout: ' + data.toString());
-    });
+        proc.stderr.on('data', function (data) {
+            response.write('stderr: ' + data.toString());
+            response.end('end');
+        });
 
-    proc.stderr.on('data', function (data) {
-        response.write('stderr: ' + data.toString());
-        response.end('end');
-    });
+        proc.on('close', function(code) {
+            response.write('closed with code 2' + code);
+            response.end('end');
+        });
 
-    proc.on('close', function(code) {
-        response.write('closed with code 2' + code);
-        response.end('end');
     });
 }).listen(8080);
 
